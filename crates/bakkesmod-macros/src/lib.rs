9ac1cf61ad07c5e2ec26e0a6449dec16c18658ab
Arator <arator.rl@outlook.com>
2020-09-03 00:46:41 +0200

    Add info to manifest

--- crates/bakkesmod-macros/src/lib.rs
+++ crates/bakkesmod-macros/src/lib.rs
@@ -1,15 +1,11 @@
-#![allow(unused)]
 extern crate proc_macro;
-use proc_macro2::{Ident, Span};
 
 use proc_macro::TokenStream;
 use quote::quote;
-use quote::ToTokens;
-use syn::ItemFn;
 
 #[proc_macro_attribute]
-pub fn plugin_init(attr: TokenStream, input: TokenStream) -> TokenStream {
-    let mut parsed_input: syn::ItemFn = syn::parse_macro_input!(input as syn::ItemFn);
+pub fn plugin_init(_attr: TokenStream, input: TokenStream) -> TokenStream {
+    let parsed_input: syn::ItemFn = syn::parse_macro_input!(input as syn::ItemFn);
     let name = parsed_input.clone().sig.ident;
 
     let tokens = quote! {
