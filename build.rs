40f3468556bf03ed5a4841dd3bec0c7c9e49d86f
Arator <arator.rl@outlook.com>
2020-09-12 15:06:57 +0200

    Don't require pluginsdk.lib when building docs on docs.rs

--- build.rs
+++ build.rs
@@ -1,5 +1,9 @@
 use std::env;
 
+#[cfg(feature = "docsrs")]
+fn main() {}
+
+#[cfg(not(feature = "docsrs"))]
 fn main() {
     let bakkesmod_path = env::var("BAKKESMOD_LIB_PATH")
         .expect("enviroment variable 'BAKKESMOD_LIB_PATH' not set!");
