dd448790263731787771760605f6da30cd8f8547
Arator <arator.rl@outlook.com>
2020-09-12 15:31:10 +0200

    Fix build cfg

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
 use std::env;
 
-#[cfg(feature = "docsrs")]
+#[cfg(docsrs)]
 fn main() {}
 
-#[cfg(not(feature = "docsrs"))]
+#[cfg(not(docsrs))]
 fn main() {
     let bakkesmod_path = env::var("BAKKESMOD_LIB_PATH")
         .expect("enviroment variable 'BAKKESMOD_LIB_PATH' not set!");
